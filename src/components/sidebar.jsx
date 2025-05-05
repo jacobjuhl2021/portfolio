63ad72d2c6ce259d342cdb87850ef0e2b06f2ea7
Jacob Juhl <jacobjuhlprivat@gmail.com>
2025-05-05 17:56:30 +0200

    Fix import path for Sidebar.module.css

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 // src/components/Sidebar.js
 import React from 'react';
-import styles from './Sidebar.module.css'; 
+import styles from './sidebar.module.css'; 
 
 export default function Sidebar({ setActiveProject, activeProject }) { 
   return (
